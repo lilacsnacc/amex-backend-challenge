46abcfcc1d3fa5c8b8e20d89d349ac9bd9884737
lilacsnacc <lilacsnacc@gmail.com>
2025-11-09 18:52:27 -0500

    addEvent will now retry 3 times over 6 seconds, code is formatted to suite standard tastes, code error corrected

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # EMS API
-Welcome to the Event Management System (EMS) API. Use this API to get users, events, and events by user. You can also add events by passing an event name and user id. Feel free to to contact me at fullstack@nshabazz.com if you have any questions (or job offers :). Happy Coding!
+Welcome to the Event Management System (EMS) API. Use this API to get users, events, and events by user. You can also add events by passing an event name and user id. Feel free to to contact me at fullstack@nshabazz.com if you have any questions (or job offers :D). Happy Coding!
 
 ## Installation
 This project is written in Node.js
@@ -71,6 +71,8 @@ and will return `{"success":true}` if successful.
 5:00 - fleshed out README file
 5:30 - refactored `/getEventsByUserId/:id` endpoint to use `Promise.allSettled()`
 6:15 - standardized `GET` fetch function and reordered code for better DX
+6:30 - added retry code and cleanup to the `addEvent` endpoint
+6:45 - some code cleanup to be more palatable to most developers (semicolons :P), plus a correction to error code.
 
 ## Given More Time...
 Since the backend is a mocked server, there is some weirdness and best practices cannot be put to use. For example, instead of returning an empty array, calling for an id that is out of bounds used to actually cause an error requiring a server restart. I'd also like to add some catches, but without proper server structure, it's difficult to test those.
--- services/index.js
+++ services/index.js
@@ -1,61 +1,72 @@
-const fastify = require('fastify')({ logger: true })
-const listenMock = require('../mock-server')
+const fastify = require('fastify')({ logger: true });
+const listenMock = require('../mock-server');
 
 /** standardized fetch */
-const jsonFetch = async (url, options) => fetch(url, options).then(resp => resp.json())
+const jsonFetch = async (url, options) => fetch(url, options).then(resp => resp.json());
 
 fastify.get('/getUsers', async (_, reply) => {
-  const users = await jsonFetch('http://event.com/getUsers')
-  reply.send(users)
-})
+  const users = await jsonFetch('http://event.com/getUsers');
+  reply.send(users);
+});
 
 fastify.get('/getEvents', async (_, reply) => {
-  const events = await jsonFetch('http://event.com/getEvents')
-  reply.send(events)
-})
+  const events = await jsonFetch('http://event.com/getEvents');
+  reply.send(events);
+});
 
 fastify.get('/getEventsByUserId/:id', async (request, reply) => {
-  const { id } = request.params
-  const eventArray = []
+  const { id } = request.params;
+  const eventArray = [];
 
   await jsonFetch(`http://event.com/getUserById/${id}`)
     .then(async user => {
-      if (!user?.events?.length) return 0
+      if (!user?.events?.length) return 0;
 
-      const fetchArr = user.events.map(ev => jsonFetch(`http://event.com/getEventById/${ev}`))
+      const fetchArr = user.events.map(ev => jsonFetch(`http://event.com/getEventById/${ev}`));
 
       await Promise
         .allSettled(fetchArr)
         .then(respArr => respArr.filter(resp => resp.status == 'fulfilled'))
         .then(filteredRespArr => filteredRespArr.map(resp => resp.value))
-        .then(events => eventArray.push(...events))
+        .then(events => {
+          eventArray.push(...events);
+          reply.send(eventArray);
+        });
     })
-    .catch(e => console.error(e))
-
-  reply.send(eventArray)
-})
+    .catch(err => {
+      console.error(err);
+      reply.send(err);
+    });
+});
 
 fastify.post('/addEvent', async (request, reply) => {
-  try {
-    const resp = await fetch('http://event.com/addEvent', {
+  const maxTries = 3;
+
+  let resp = {};
+
+  for (let i = 0; (i < maxTries) && !resp.success; i++) {
+    resp = await jsonFetch('http://event.com/addEvent', {
       method: 'POST',
       body: JSON.stringify({
         id: new Date().getTime(),
         ...request.body
       })
-    })
-    const data = await resp.json()
-    reply.send(data)
-  } catch (err) {
-    reply.error(err)
+    });
+
+    if (!resp.success) await new Promise(resolve => setTimeout(resolve, 1000 * (i + 1)));
   }
-})
+
+  if (!resp.success)
+    resp.message = `Retried ${maxTries} times unsuccessfully - please try again in 30 seconds. ${resp.message}`;
+
+  reply.send(resp);
+});
 
 fastify.listen({ port: 3000 }, (err) => {
-  listenMock()
+  listenMock();
 
   if (err) {
-    fastify.log.error(err)
-    process.exit()
+    fastify.log.error(err);
+    process.exit();
   }
-})
+});
